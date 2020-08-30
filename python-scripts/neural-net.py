67202de7b71ebbf9d64eb7d3eda7538270d8f6b6
KeitaIsFree <maxdxd10108102@gmail.com>
2020-08-30 21:35:01 +0900

    User input management

--- python-scripts/neural-net.py
+++ python-scripts/neural-net.py
@@ -1,3 +1,15 @@
 #! /usr/bin/env python3
 
 print('Hi, I am a chess bot!')
+
+def main():
+    done = False
+    user_input = ''
+    while not done:
+        user_input = input('Please input move: ')
+        if user_input=='E':
+            done = True
+        print('Your input was: '+user_input)
+
+if __name__=='__main__':
+    main()
