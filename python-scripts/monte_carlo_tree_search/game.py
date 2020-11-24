f9ba38f9c0e112b1d094e89b17651fa7b627a6a8
frozenegg <60460979+frozenegg@users.noreply.github.com>
2020-11-24 23:11:57 +0900

    modify game to chess game

--- python-scripts/monte_carlo_tree_search/game.py
+++ python-scripts/monte_carlo_tree_search/game.py
@@ -1,72 +1,43 @@
 import random
 import math
+import chess
 
 class State:
-	def __init__(self, pieces=None, enemy_pieces=None):
-		self.pieces = pieces if pieces != None else [0] * 9
-		self.enemy_pieces = enemy_pieces if enemy_pieces != None else [0] * 9
-
-	def piece_count(self, pieces):
-		count = 0
-		for i in pieces:
-			if i == 1:
-				count += 1
-		return count
+	def __init__(self):
+		self.board = chess.Board()
 
 	def is_lose(self):
-		def is_comp(x, y, dx, dy):
-			for k in range(3):
-				if y < 0 or 2 < y or x < 0 or 2 < x or self.enemy_pieces[x+y*3] == 0:
-					return False
-				x, y = x + dx, y + dy
-			return True
-
-		if is_comp(0,0,1,1) or is_comp(0,2,1,-1):
-			return True
-		for i in range(3):
-			if is_comp(0,i,1,0) or is_comp(i,0,0,1):
-				return True
-
-		return False
+		return
 
 	def is_draw(self):
-		return self.piece_count(self.pieces) + self.piece_count(self.enemy_pieces) == 9
+		return self.board.is_stalemate() and self.board.is_insufficient_material()
 
 	def is_done(self):
-		return self.is_lose() or self.is_draw()
+		return self.board.is_game_over()
 
 	def next(self, action):
-		pieces = self.pieces.copy()
-		pieces[action] = 1
-		return State(self.enemy_pieces, pieces)
+		move = chess.Move.from_uci(action)
+		self.board.push(move)
 
 	def legal_actions(self):
-		actions = []
-		for i in range(9):
-			if self.pieces[i] == 0 and self.enemy_pieces[i] == 0:
-				actions.append(i)
-		return actions
+		legal_actions = []
+
+		for i in board.legal_moves:
+	  		legal_actions.append(str(i))
+
+		return legal_actions
 
 	def is_first_player(self):
-		return self.piece_count(self.pieces) == self.piece_count(self.enemy_pieces)
-
-	def __str__(self):
-		ox = ('o', 'x') if self.is_first_player() else ('x', 'o')
-		str = ''
-		for i in range(9):
-			if self.pieces[i] == 1:
-				str += ox[0]
-			elif self.enemy_pieces[i] == 1:
-				str += ox[1]
-			else:
-				str += '-'
-			if i %3 == 2:
-				str += '\n'
-		return str
+		return board.turn == chess.WHITE
 
 def random_action(state):
-	legal_actions = state.legal_actions()
+	legal_actions = []
+
+	for i in state.board.legal_moves:
+	  	legal_actions.append(str(i))
+
 	return legal_actions[random.randint(0, len(legal_actions)-1)]
+
 
 if __name__ == '__main__':
 	state = State()
@@ -75,7 +46,7 @@ if __name__ == '__main__':
 		if state.is_done():
 			break
 
-		state = state.next(random_action(state))
+		state.next(random_action(state))
 
-		print(state)
+		print(state.board)
 		print()
